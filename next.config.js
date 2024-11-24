80c6bfcb5f5c62ab61972ca7092e51fa7227e084
Raul Primo <raul.primoimp@gmail.com>
2024-11-24 14:10:36 -0300

    bug fix #3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,21 @@
 const nextConfig = {
   images: {
     domains: [],
-  }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ttf|woff|woff2|eot)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          publicPath: '/_next/static/fonts/',
+          outputPath: 'static/fonts/',
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
