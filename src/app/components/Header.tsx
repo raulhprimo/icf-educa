7de9c6f0851799e4b7d8f719505ccc9c6b54f0ab
Raul Primo <raul.primoimp@gmail.com>
2024-11-24 13:09:06 -0300

    add fonts

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,16 @@
-
+import Image from 'next/image'
 
 export function Header() {
   return (
     <header className="flex justify-between items-center px-8 py-4 bg-[#212121]">
       <div className="flex items-center gap-2">
-        <div className="w-10 h-10 bg-white" /> {/* Placeholder para o logo */}
+        <Image 
+          src="/logo_icf.svg"
+          alt="Logo ICF Educa"
+          width={40}
+          height={40}
+          className="object-contain"
+        />
         <h1 className="text-white text-2xl font-bold">ICF EDUCA</h1>
       </div>
       <p className="text-white text-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        primary: '#2563eb',
-        secondary: '#475569',
-      }
+      fontFamily: {
+        sans: ['var(--font-futura)'],
+      },
     },
   },
   plugins: [],
